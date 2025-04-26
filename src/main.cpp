572bab196ed692ab851dc1a0ccdc953bb278b65d
Youssef Eddaif <183735203+j0d3v@users.noreply.github.com>
2025-04-26 22:56:28 +0100

    Add Snake to main with movement and rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
+#include "Snake.hpp"
 #include <SFML/Graphics.hpp>
+#include <SFML/Window/VideoMode.hpp>
+#include <map>
+#include <optional>
 
 int main() {
-  auto window =
-      sf::RenderWindow(sf::VideoMode({800u, 800u}), "CMake SFML Project");
-  window.setFramerateLimit(144);
+  sf::RenderWindow window(sf::VideoMode({400, 400}), "SFML");
+  window.setFramerateLimit(60);
+
+  Snake snake(20);
+  const std::map<sf::Keyboard::Scan, sf::Vector2i> directions = {
+      {sf::Keyboard::Scan::Right, {1, 0}},
+      {sf::Keyboard::Scan::Left, {-1, 0}},
+      {sf::Keyboard::Scan::Up, {0, -1}},
+      {sf::Keyboard::Scan::Down, {0, 1}}};
 
   while (window.isOpen()) {
-    while (const std::optional event = window.pollEvent()) {
-      if (event->is<sf::Event::Closed>()) {
+    while (auto eventOpt = window.pollEvent()) {
+      const sf::Event &event = *eventOpt;
+
+      if (event.is<sf::Event::Closed>()) {
         window.close();
+      }
+
+      if (auto keyPressed = event.getIf<sf::Event::KeyPressed>()) {
+        auto it = directions.find(keyPressed->scancode);
+        if (it != directions.end()) {
+          snake.turn(it->second);
+          snake.move();
+        }
       }
     }
 
     window.clear();
+    snake.render(window);
     window.display();
   }
 }
