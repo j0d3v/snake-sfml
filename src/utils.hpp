138af6f645a6a4b322b9a61515724ae0ec894508
Youssef Eddaif <183735203+j0d3v@users.noreply.github.com>
2025-04-26 22:52:28 +0100

    Add utility for random positions

---
+++ src/utils.hpp
@@ -0,0 +1,16 @@
+#ifndef __UTILS__
+#define __UTILS__
+
+#include <SFML/System/Vector2.hpp>
+#include <cstdlib>
+#include <ctime>
+
+const int BOARD_SIZE = 400;
+
+inline sf::Vector2f getRandomPosition(int width, int height) {
+    float x = static_cast<float>(std::rand() % width);
+    float y = static_cast<float>(std::rand() % height);
+    return sf::Vector2f(x, y);
+}
+
+#endif
